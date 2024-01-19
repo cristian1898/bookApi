9a23b4ca55e43363ef9f2d94bc190a0eb9bee8dc
ccuray <cristianpaul.curaycuray@emeal.nttdata.com>
2024-01-19 05:57:30 +0400

    Chore : add files

---
+++ src/services/data/fakeDataAuthor.ts
@@ -0,0 +1,202 @@
+export const AuthorDataFake = [
+  {
+    id: "f056972c-5c3d-49d6-99b2-f1f1586744df",
+    name: "Leo Hudson",
+    email: "Tyshawn22@yahoo.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/958.jpg",
+    biography_information:
+      "Acquiro adaugeo cibus tabgo ratione videlicet.\nDemo conqueror ulterius odio sed voluntarius subseco trucido tonsor asper.\nDelibero vivo vilitas.\nCetera stillicidium coadunatio.\nDolorem thesis animus aveho succedo vindico.\nPatior aggredior umbra currus quibusdam angustus terebro allatus.\nPaens umquam nisi victus arceo clamo tabernus.\nCrinis territo depromo absconditus decretum blanditiis vehemens minus caelestis.",
+    first_name: "Dolores",
+    last_name: "Pfannerstill",
+  },
+  {
+    id: "79614943-95ae-404f-8158-91db5467afa3",
+    name: "Adrienne Brekke",
+    email: "Vernie_Doyle88@yahoo.com",
+    img: "https://avatars.githubusercontent.com/u/45574891",
+    biography_information:
+      "Ventito hic ullam conqueror consequatur id patior thesaurus.\nStipes tergeo cunctatio cedo carmen corpus curiositas.\nInventore possimus succurro unde cohors video vulgivagus tredecim.\nSunt blandior cunctatio non.\nTenus currus arguo voveo cornu terra cuppedia cubitum corpus.\nSponte desparatus contra denique.\nDoloremque verto ater crastinus deficio desidero accedo demum administratio vetus.\nTriumphus adaugeo commodi fuga accusamus alii caste decretum cribro.",
+    first_name: "Tad",
+    last_name: "Weimann",
+  },
+  {
+    id: "0b73fb9c-98d6-410f-86c2-189f1c091058",
+    name: "Kristin Marquardt",
+    email: "Everette.Kirlin90@yahoo.com",
+    img: "https://avatars.githubusercontent.com/u/18155658",
+    biography_information:
+      "Ullus adulescens ceno torqueo suppono.\nContego eveniet auxilium cuppedia stillicidium.\nAudacia decretum fuga cena est curiositas crux supplanto laborum volo.\nMagni celebrer vulnus curiositas confido depereo pecus color.\nStudio dignissimos celo.\nCinis accusamus valetudo adiuvo.\nVerecundia curis teres attero.\nAttero animadverto suggero suasoria.",
+    first_name: "Percy",
+    last_name: "Jerde",
+  },
+  {
+    id: "76f7ff69-4af7-4083-bbc7-524cb3f7383c",
+    name: "Christian Kertzmann",
+    email: "Burdette90@gmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/557.jpg",
+    biography_information:
+      "Urbs cupiditate decor.\nCattus possimus vulgus tum varietas video capio canis.\nTubineus arcus calco tempus ipsam veritatis traho.\nAppono umquam cohors video aliqua solum testimonium aegrus bonus.\nUtilis ocer tandem.\nTonsor quibusdam depraedor voluptatem tergo truculenter coepi spoliatio consectetur.\nThesaurus altus totam curia creo.\nVobis soluta agnitio aegrotatio illum vallum non tunc talio.",
+    first_name: "Katrine",
+    last_name: "Block-Walter",
+  },
+  {
+    id: "e66baceb-d04d-4d7f-a717-877051240c15",
+    name: "Rudy Frami",
+    email: "Albina.Fritsch81@yahoo.com",
+    img: "https://avatars.githubusercontent.com/u/3653863",
+    biography_information:
+      "Comptus angelus textus appello placeat amissio.\nCunabula quos depopulo vitiosus corrumpo.\nCaries suscipit solio.\nAbsorbeo vespillo thymbra cultellus complectus approbo.\nAdulescens synagoga condico adulescens cunctatio una.\nDecretum delectus urbanus caterva aequus tabernus.\nDeficio verecundia denuo.\nAcceptus curriculum sint adficio quibusdam vomito sortitus vomito cimentarius.",
+    first_name: "Blake",
+    last_name: "McCullough",
+  },
+  {
+    id: "b35b1f20-b6ff-4b4e-ae3d-b6d4f5c059ec",
+    name: "Jasmine Hammes",
+    email: "Amanda_Marvin38@gmail.com",
+    img: "https://avatars.githubusercontent.com/u/10712409",
+    biography_information:
+      "Ago coruscus adinventitias cauda adsum.\nAdministratio textilis dolor vulgus.\nCuius amplexus thermae artificiose demoror depereo turbo deserunt sponte.\nCommodi victus corrigo acies cometes tumultus eligendi vilis eveniet cotidie.\nVallum viscus adsuesco cauda ustulo arguo amitto venustas verus cilicium.\nAdversus adeptio thesis thermae amitto absque.\nCubo ademptio cribro tamquam sophismata vinculum tres strenuus.\nClamo carus sol quo studio ustilo ante culpa.",
+    first_name: "Wendell",
+    last_name: "Rutherford",
+  },
+  {
+    id: "92f5f6ee-adb2-4f7d-adb5-f4d0367fa615",
+    name: "Antonio Hintz",
+    email: "Ressie_OKon88@yahoo.com",
+    img: "https://avatars.githubusercontent.com/u/72268787",
+    biography_information:
+      "Ipsum vilitas cubitum vae.\nDemum surculus carus adficio utrimque sint.\nCommodo spero sonitus.\nClibanus spes velociter administratio amor.\nAntepono animadverto vulgo cavus decerno aetas sponte tardus eaque certe.\nCreber nam vulticulus vehemens canis suscipit contabesco sublime.\nVereor admiratio torqueo vulariter provident cras.\nBardus solitudo facilis solitudo voluptate tremo sponte exercitationem volubilis dolores.",
+    first_name: "Cornelius",
+    last_name: "Stoltenberg",
+  },
+  {
+    id: "2af0c7e5-5c00-4a0a-ae3e-ac5e1b758a57",
+    name: "Tim Leannon",
+    email: "Jaquelin18@yahoo.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/286.jpg",
+    biography_information:
+      "Strues confido paulatim.\nCommunis iure quaerat cariosus concedo.\nReprehenderit nisi antiquus attero audacia molestiae super acidus.\nAlter amitto celer.\nAnser veritas talus cultellus ultio cruentus.\nVivo somnus succedo arbustum.\nDebilito crux sollers blanditiis tolero contra qui aperte.\nArma vado tepesco stella corrigo absorbeo debeo sufficio.",
+    first_name: "Evalyn",
+    last_name: "Wiza",
+  },
+  {
+    id: "3bc1f13b-4244-4f20-82ee-ed7783de31ad",
+    name: "Dr. Victor Langworth",
+    email: "Demond_Denesik79@gmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1070.jpg",
+    biography_information:
+      "Cohaero conturbo curvo clementia quasi coepi delego cribro beatae despecto.\nOdio anser sopor alter tantillus cohaero exercitationem sollicito summisse.\nSpargo dens aufero.\nUllus vesica aperiam thymbra eaque celebrer consectetur arma.\nRepudiandae tutis basium vilis beatae.\nTyrannus adhaero pel thermae quis turba conventus decerno crudelis.\nMolestiae accusamus tui clamo repudiandae animadverto confero.\nDefaeco enim optio cauda creptio victus tondeo.",
+    first_name: "Wilford",
+    last_name: "Block",
+  },
+  {
+    id: "c9bd0a27-3bf2-45c9-91cf-2683f5ff93a0",
+    name: "Wayne Grady",
+    email: "Heaven.Effertz@hotmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/654.jpg",
+    biography_information:
+      "Ab quibusdam stella tener pectus amoveo iure tricesimus asporto.\nTendo concedo id cogito admitto excepturi ex appello.\nCubitum cribro dolorum.\nInflammatio vergo artificiose quidem.\nBlandior crudelis explicabo.\nCapillus sto creo aro cetera dolorem.\nTener vulgus comedo.\nVoluptatibus est pel clibanus.",
+    first_name: "Queenie",
+    last_name: "Willms",
+  },
+  {
+    id: "9060cce8-f282-4e97-a75d-5b2a02c54940",
+    name: "Cameron Gulgowski",
+    email: "Annie.Smitham@hotmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/828.jpg",
+    biography_information:
+      "Tripudio aurum adopto thema.\nCohibeo pectus curo.\nLaborum ver amicitia curo spectaculum adicio defero thema viriliter.\nCopia caries depono.\nClamo clibanus assumenda solvo.\nTremo contigo solio validus coruscus sonitus.\nConduco vulariter brevis.\nDedico demonstro trucido ullam commemoro sum aegrus neque cognomen ullam.",
+    first_name: "Lafayette",
+    last_name: "Koepp",
+  },
+  {
+    id: "58cbcbb6-a276-4c34-a978-32815a09cd20",
+    name: "Rochelle Kautzer",
+    email: "Deondre65@hotmail.com",
+    img: "https://avatars.githubusercontent.com/u/23611735",
+    biography_information:
+      "Audacia sodalitas una temperantia.\nVir beneficium crebro vobis.\nCrapula campana desparatus speciosus quisquam.\nAdmiratio vetus tenus suus sunt absorbeo totus fuga.\nVilla vobis compono vulgaris cunabula vinculum decet tabella adsum usus.\nAntiquus abduco neque.\nEarum cometes conatus mollitia cubicularis vilicus adhaero cultura vos tutis.\nOdio adficio alius voluptates.",
+    first_name: "Rickie",
+    last_name: "Hirthe",
+  },
+  {
+    id: "401ab78b-9db6-4803-ae54-5ab4749cf422",
+    name: "Max Gleason Sr.",
+    email: "Timothy_Dare98@gmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/249.jpg",
+    biography_information:
+      "Celo abduco atqui.\nCruciamentum aedificium repellendus abstergo.\nVenustas auditor amplexus constans aggero voluptates calco verus suscipio.\nDeprecator velociter quasi statim solutio rerum celo cimentarius.\nArguo curto auxilium blandior.\nCohors vindico cogo tribuo vestigium curis desipio tunc coruscus.\nCoruscus vito audax desolo delectus cervus tepesco ancilla beneficium volaticus.\nCondico comparo solio adfectus volaticus valens surgo.",
+    first_name: "Charlene",
+    last_name: "Harber",
+  },
+  {
+    id: "054ebe2a-bb95-4687-8fdc-9b368fcf3f0b",
+    name: "Camille Pfeffer",
+    email: "Jamil.Shields63@yahoo.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1152.jpg",
+    biography_information:
+      "Timor adicio ad agnosco distinctio.\nNumquam solutio sperno tricesimus caterva constans aveho crastinus argumentum.\nComprehendo aptus carbo abeo tollo vehemens nostrum sum.\nCinis quae ustulo canis vito suffragium articulus deleniti curo.\nVulariter aeternus credo cernuus adulescens illo vir vulnus.\nPerferendis apostolus copia compello fuga.\nArmarium ceno cernuus sumo tabula vestigium.\nDolorum tactus thema adinventitias cras error civitas traho voluptas.",
+    first_name: "Vernice",
+    last_name: "Kilback",
+  },
+  {
+    id: "dc9bd9d5-575b-44da-8f5c-fb209880aec1",
+    name: "Cornelius Deckow",
+    email: "Dayana_Gottlieb@hotmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/464.jpg",
+    biography_information:
+      "Cum asperiores cauda calcar denuncio deripio.\nSpeculum crux abundans odit vacuus verumtamen.\nAbsque suadeo allatus desipio.\nAvarus desino sumo capio pecco pectus corrigo tergiversatio possimus certe.\nAutus amplitudo ad.\nVilla acidus nisi caute vigor cotidie ea claudeo vos illo.\nAegre carbo derideo vita placeat.\nDenuo veritas compono.",
+    first_name: "Judson",
+    last_name: "Hand",
+  },
+  {
+    id: "5d19228a-eca5-4e76-9937-ab4594bcb2b8",
+    name: "Gordon Rippin",
+    email: "Audrey_Kuhn@gmail.com",
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/894.jpg",
+    biography_information:
+      "Incidunt dapifer umbra vociferor vinum bonus adamo.\nConicio dolore accendo cribro.\nSpeculum aveho vallum.\nTotidem urbanus tenax culpo aggero vigor curto admiratio.\nAssumenda appello illo sordeo voluptas viduo surgo.\nTriduana ex amicitia tabula adopto.\nDesipio canto cras altus tertius ventosus.\nVita vacuus pel conculco adulatio.",
+    first_name: "Josephine",
+    last_name: "Stark",
+  },
+  {
+    id: "73e974ba-fa05-47d1-9393-3482ede3e2e2",
+    name: "Wm Marvin",
+    email: "Connor91@hotmail.com",
+    img: "https://avatars.githubusercontent.com/u/94409171",
+    biography_information:
+      "Similique conatus conspergo coruscus illo incidunt stipes.\nCruentus venia approbo accusator crustulum bis.\nAd desino tonsor.\nVotum sapiente attonbitus tantillus capio delicate ex tempora tres thorax.\nEligendi sui cattus audax unde placeat suscipit una.\nUmquam creta rem vigor comedo perspiciatis conatus exercitationem defleo qui.\nSol vestrum dedico curatio vulnus verbum vae.\nVulgivagus ceno cado adicio adduco.",
+    first_name: "Alyce",
+    last_name: "Lind",
+  },
+  {
+    id: "770ff491-181f-400e-8b69-b15d00bb7c69",
+    name: "Earl Hagenes",
+    email: "Ward_Wintheiser48@gmail.com",
+    img: "https://avatars.githubusercontent.com/u/35794142",
+    biography_information:
+      "Decet volva tabella videlicet carcer conatus.\nCimentarius denuo copiose alveus.\nArgumentum maiores velut tricesimus civis.\nCicuta soluta velut supellex tibi tabula terreo.\nComes arbitro comedo.\nDepulso voluptates deputo.\nTres peior repellat vis.\nCrebro aeger deinde spoliatio excepturi utor atrocitas acerbitas pariatur.",
+    first_name: "Warren",
+    last_name: "Swaniawski",
+  },
+  {
+    id: "c96715f9-8f4c-4e02-8c48-3a2faa252cb0",
+    name: "Leonard Leffler-Daniel",
+    email: "Martine_Runolfsson89@gmail.com",
+    img: "https://avatars.githubusercontent.com/u/26260455",
+    biography_information:
+      "Quibusdam volaticus somnus amo eius clam.\nAngustus amitto vulnero.\nViriliter curia cuppedia consuasor catena degero.\nConatus tunc magnam adfectus.\nTero cresco tantum adulatio vilis terra brevis spectaculum.\nPecco vobis temeritas repellendus decens vigilo acies barba.\nConvoco velit conventus textus maiores ab commemoro ascisco adsum tyrannus.\nArchitecto tempore depraedor cuius.",
+    first_name: "Hilma",
+    last_name: "Leuschke",
+  },
+  {
+    id: "a11bb748-4c68-49d2-890d-b9697b53e088",
+    name: "Janet Shields",
+    email: "Emerson_Gutkowski42@yahoo.com",
+    img: "https://avatars.githubusercontent.com/u/58865839",
+    biography_information:
+      "Adfectus occaecati sperno cinis.\nDeduco subnecto antepono minima vel crastinus aranea.\nModi ultra pax vulgus corrigo eveniet inflammatio sursum.\nCaelum asperiores voluntarius coruscus consequatur absque aperio aggredior.\nAngulus videlicet itaque dolor deserunt beneficium depraedor pectus.\nUmerus ara ambitus territo dignissimos.\nAbundans decet vergo comburo beatae vitae campana nobis tubineus dolore.\nCur videlicet crustulum ad incidunt cohibeo neque.",
+    first_name: "London",
+    last_name: "Fay",
+  },
+];
